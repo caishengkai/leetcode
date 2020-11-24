310191c437d71b80b583f42ae998b7be4d6cfb84
caishengkai <W1iokKqr5nnuBVEp>
2020-11-24 22:12:40 +0800

    solution147 change class name

--- src/com/csk/leetcode/Solution147.java
+++ src/com/csk/leetcode/Solution147.java
@@ -7,20 +7,20 @@ package com.csk.leetcode;
  */
 public class Solution147 {
 
-    public ListNode insertionSortList(ListNode head) {
+    public ListNode147 insertionSortList(ListNode147 head) {
         if (head == null || head.next == null) {
             return head;
         }
         //定义已排序链表的最后一个节点（也就是最大节点），初始为第一个节点
-        ListNode lastSorted = head;
+        ListNode147 lastSorted = head;
         //定义当前要插入的节点，初始为第二个节点
-        ListNode curr = head.next;
+        ListNode147 curr = head.next;
         while (curr != null) {
             //如果要插入的节点比已排序的最大节点还要大，将lastSorted移到curr位置
             if (lastSorted.val <= curr.val) {
                 lastSorted = curr;
             } else { //从头开始遍历，找到第一个比curr大的，插入到它前面
-                ListNode prev = head;
+                ListNode147 prev = head;
                 //如果curr比头节点还要小，就插入到头节点
                 if (curr.val <= prev.val) {
                     lastSorted.next = curr.next;
@@ -48,19 +48,19 @@ public class Solution147 {
      * @param head
      * @return
      */
-    public ListNode insertionSortList1(ListNode head) {
+    public ListNode147 insertionSortList1(ListNode147 head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode dummyHead = new ListNode(0);
+        ListNode147 dummyHead = new ListNode147(0);
         dummyHead.next = head;
-        ListNode lastSorted = head;
-        ListNode curr = head.next;
+        ListNode147 lastSorted = head;
+        ListNode147 curr = head.next;
         while (curr != null) {
             if (lastSorted.val <= curr.val) {
                 lastSorted = curr;
             } else {
-                ListNode prev = dummyHead;
+                ListNode147 prev = dummyHead;
                 while (prev.next.val <= curr.val) {
                     prev = prev.next;
                 }
@@ -75,10 +75,10 @@ public class Solution147 {
 
     public static void main(String[] args) {
         Solution147 solution = new Solution147();
-        ListNode node = new ListNode(4);
-        node.next = new ListNode(3);
-        node.next.next = new ListNode(2);
-        node.next.next.next = new ListNode(1);
+        ListNode147 node = new ListNode147(4);
+        node.next = new ListNode147(3);
+        node.next.next = new ListNode147(2);
+        node.next.next.next = new ListNode147(1);
         node = solution.insertionSortList1(node);
         System.out.print(node.val + "->");
         System.out.print(node.next.val + "->");
@@ -87,8 +87,8 @@ public class Solution147 {
     }
 }
 
-class ListNode {
+class ListNode147 {
     int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
+    ListNode147 next;
+    ListNode147(int x) { val = x; }
 }
