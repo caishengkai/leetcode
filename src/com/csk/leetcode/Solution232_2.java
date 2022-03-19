f40c2337364e577c241817ddcd08f086e0e48d5d
caishengkai <312039449@qq.com>
2022-03-19 15:38:48 +0800

    2个栈转换为队列 2022.03.19

---
+++ src/com/csk/leetcode/Solution232_2.java
@@ -0,0 +1,99 @@
+package com.csk.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @description: 用栈实现队列
+ * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列的支持的所有操作（push、pop、peek、empty）：
+ *
+ * 实现 MyQueue 类：
+ *
+ * void push(int x) 将元素 x 推到队列的末尾
+ * int pop() 从队列的开头移除并返回元素
+ * int peek() 返回队列开头的元素
+ * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+ * 
+ *
+ * 说明：
+ *
+ * 你只能使用标准的栈操作 —— 也就是只有push to top,peek/pop from top,size, 和is empty操作是合法的。
+ * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ * 
+ *
+ * 进阶：
+ *
+ * 你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
+ * 
+ *
+ * 示例：
+ *
+ * 输入：
+ * ["MyQueue", "push", "push", "peek", "pop", "empty"]
+ * [[], [1], [2], [], [], []]
+ * 输出：
+ * [null, null, null, 1, 1, false]
+ *
+ * 解释：
+ * MyQueue myQueue = new MyQueue();
+ * myQueue.push(1); // queue is: [1]
+ * myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+ * myQueue.peek(); // return 1
+ * myQueue.pop(); // return 1, queue is [2]
+ * myQueue.empty(); // return false
+ * 
+ *
+ * 提示：
+ *
+ * 1 <= x <= 9
+ * 最多调用 100 次 push、pop、peek 和 empty
+ * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）
+ *
+ * 思路：
+ * 第一个栈负责插入，第二个栈负责弹出； 当第二个栈为空时，将第一个栈弹出插入到第二栈中
+ *
+ * @author: caishengkai
+ * @date: 2020-12-28 11:49
+ */
+public class Solution232_2 {
+
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
+
+    public Solution232_2() {
+        stack1 = new ArrayDeque<>();
+        stack2 = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return stack2.pop();
+    }
+
+    public int peek() {
+        while (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Solution232_2 solution = new Solution232_2();
+        solution.push(1);
+        solution.push(2);
+        System.out.println(solution.peek());
+        System.out.println(solution.pop());
+        System.out.println(solution.empty());
+    }
+}
